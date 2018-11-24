441bc94a5df9c33436c3670dd6a1e1fff97e4de5
VicenteBarria <v.barriaveas@gmail.com>
2018-11-23 22:37:22 -0300

    no more psql

--- Gomitas/src/main/java/com/gomitas/Gomitas/GomitasController.java
+++ Gomitas/src/main/java/com/gomitas/Gomitas/GomitasController.java
@@ -1,13 +1,13 @@
 package com.gomitas.Gomitas;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import java.util.List;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
+//import org.springframework.hateoas.Resource;
+//import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
